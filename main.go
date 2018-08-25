e92a1dd2712b2786b7a31b0a49a914a7eafb6322
Colton Robertson <colton.robertson@rewardstyle.com>
2018-08-25 12:25:52 -0500

    Slowly building out the map based on the string
    Not sure if this is the best way to go about it.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"strings"
 )
 
@@ -9,28 +10,49 @@ func describe(i interface{}) {
 	fmt.Printf("(%v, %T)\n", i, i)
 }
 
-func buildMap(keys []string, v map[string]interface{}) {
-	if len(keys) == 0 {
+func Foo(key string, v map[string]interface{}) map[string]interface{} {
+	if x, ok := v[key]; ok {
+		return x.(map[string]interface{})
+	} else {
+		v[key] = make(map[string]interface{})
+		return v[key].(map[string]interface{})
+	}
+}
+
+func buildMap(keys []string, v map[string]interface{}) (string, map[string]interface{}) {
+	var key string
+	for _, key = range keys {
+		v = Foo(key, v)
+	}
+
+	return key, v
+}
+
+func setValue(key string, v map[string]interface{}, value interface{}) {
+	if v[key] == nil {
+		v[key] = value
 		return
 	}
 
-	key := keys[0]
-	if x, ok := v[key]; ok {
-		buildMap(keys[1:], x.(map[string]interface{}))
-	} else {
-		v[key] = make(map[string]interface{})
-		buildMap(keys[1:], v[key].(map[string]interface{}))
+	rt := reflect.TypeOf(v[key])
+	switch rt.Kind() {
+	case reflect.Int, reflect.String:
+		oldVal := v[key]
+		v[key] = []interface{}{oldVal, value}
+	case reflect.Slice:
+		v[key] = append(v[key].([]interface{}), value)
 	}
-
 }
 
 func main() {
+	// var name string = `Colton`
+
 	keys := strings.Split("og:image:url", ":")[1:]
-	fmt.Println(keys)
 
 	x := make(map[string]interface{})
-	buildMap(keys, x)
+	key, z := buildMap(keys, x)
+	setValue(key, z, "Colton")
+	setValue(key, z, "Robertson")
 
 	fmt.Println(x)
 }
-
